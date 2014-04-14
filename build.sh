e28db596eb281990f2b641a729d2f06899b2de73
Daniel Grigg <daniel.grigg@covata.com>
2014-04-14 19:17:52 +1000

    add missing downloads folder

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@
 #  /vendor/ios/include/bullet2 - all the bullet headers
 #  /vendor/ios/lib/ - libraries for LinearMath, Collision and Dynamics.
 
+mkdir -p vendor/downloads
 cd vendor/downloads/
 rm -rf bullet2
 svn checkout http://bullet.googlecode.com/svn/trunk/ bullet2
